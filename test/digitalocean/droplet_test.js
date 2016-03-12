e02fb0206cb2a16e01de1b16877325bbe1c11b69
Phillip Baker <phillbaker@retrodict.com>
2016-03-11 19:43:52 -0500

    Add missing droplet child endpoints.
        * Kernels, Snapshots, Backups
        * Remove Actions, handled in DropletActions

--- test/digitalocean/droplet_test.js
+++ test/digitalocean/droplet_test.js
@@ -304,25 +304,76 @@ describe('droplet endpoints', function() {
     });
   });
 
-  describe('actions', function() {
+  describe('kernels', function() {
     var data = {
-      "actions": [
+      "kernels": [
         {
-          "id": 19,
-          "status": "in-progress",
-          "type": "create",
-          "started_at": "2014-07-29T14:35:39Z",
-          "completed_at": null,
-          "resource_id": 24,
-          "resource_type": "droplet",
-          "region_slug": "nyc1",
-          "region": {
-            "name": "New York",
-            "slug": "nyc1",
-            "available": true,
-            "sizes": ["512mb"],
-            "features": ["virtio", "private_networking", "backups", "ipv6", "metadata"]
-          }
+          "id": 231,
+          "name": "DO-recovery-static-fsck",
+          "version": "3.8.0-25-generic"
+        }
+      ],
+      "links": {
+        "pages": {
+          "last": "https://api.digitalocean.com/v2/droplets/3164494/kernels?page=124&per_page=1",
+          "next": "https://api.digitalocean.com/v2/droplets/3164494/kernels?page=2&per_page=1"
+        }
+      },
+      "meta": {
+        "total": 1
+      }
+    };
+
+    it('lists droplet kernels', function() {
+      testUtils.api.get('/v2/droplets/123/kernels').reply(200, JSON.stringify(data));
+
+      client.droplets.kernels(123, function(err, kernels, headers) {
+        expect(kernels).to.be.eql(data.kernels);
+      });
+    });
+
+    it('lists droplet kernels at page', function() {
+      testUtils.api.get('/v2/droplets/123/kernels?page=2').reply(200, JSON.stringify(data));
+
+      client.droplets.kernels(123, 2, function(err, droplets, headers) {
+        expect(droplets).to.be.eql(data.kernels);
+      });
+    });
+
+    it('lists droplet kernels at page with length', function() {
+      testUtils.api.get('/v2/droplets/123/kernels?page=2&per_page=1').reply(200, JSON.stringify(data));
+
+      client.droplets.kernels(123, 2, 1, function(err, droplets, headers) {
+        expect(droplets).to.be.eql(data.kernels);
+      });
+    });
+
+    it('lists droplet kernels with a query object', function() {
+      testUtils.api.get('/v2/droplets/123/kernels?page=1&per_page=1').reply(200, JSON.stringify(data));
+
+      client.droplets.kernels(123, {
+        page: 1,
+        per_page: 1
+      }, function(err, kernels, headers) {
+        expect(kernels).to.be.eql(data.kernels);
+      });
+    });
+  });
+
+  describe('snapshots', function() {
+    var data = {
+      "snapshots": [
+        {
+          "id": 119192817,
+          "name": "Ubuntu 13.04",
+          "distribution": "ubuntu",
+          "slug": null,
+          "public": true,
+          "regions": [
+            "nyc1"
+          ],
+          "created_at": "2014-07-29T14:35:40Z",
+          "type": "snapshot"
         }
       ],
       "meta": {
@@ -330,41 +381,222 @@ describe('droplet endpoints', function() {
       }
     };
 
-    it('lists droplet actions', function() {
-      testUtils.api.get('/v2/droplets/123/actions').reply(200, JSON.stringify(data));
+    it('lists droplet snapshots', function() {
+      testUtils.api.get('/v2/droplets/123/snapshots').reply(200, JSON.stringify(data));
 
-      client.droplets.actions(123, function(err, actions, headers) {
-        expect(actions).to.be.eql(data.actions);
+      client.droplets.snapshots(123, function(err, snapshots, headers) {
+        expect(snapshots).to.be.eql(data.snapshots);
       });
     });
 
-    it('lists droplet actions at page', function() {
-      testUtils.api.get('/v2/droplets/123/actions?page=2').reply(200, JSON.stringify(data));
+    it('lists droplet snapshots at page', function() {
+      testUtils.api.get('/v2/droplets/123/snapshots?page=2').reply(200, JSON.stringify(data));
 
-      client.droplets.actions(123, 2, function(err, droplets, headers) {
-        expect(droplets).to.be.eql(data.actions);
+      client.droplets.snapshots(123, 2, function(err, droplets, headers) {
+        expect(droplets).to.be.eql(data.snapshots);
       });
     });
 
-    it('lists droplet actions at page with length', function() {
-      testUtils.api.get('/v2/droplets/123/actions?page=2&per_page=1').reply(200, JSON.stringify(data));
+    it('lists droplet snapshots at page with length', function() {
+      testUtils.api.get('/v2/droplets/123/snapshots?page=2&per_page=1').reply(200, JSON.stringify(data));
 
-      client.droplets.actions(123, 2, 1, function(err, droplets, headers) {
-        expect(droplets).to.be.eql(data.actions);
+      client.droplets.snapshots(123, 2, 1, function(err, droplets, headers) {
+        expect(droplets).to.be.eql(data.snapshots);
       });
     });
 
-    it('lists droplet actions with a query object', function() {
-      testUtils.api.get('/v2/droplets/123/actions?page=1&per_page=2').reply(200, JSON.stringify(data));
+    it('lists droplet snapshots with a query object', function() {
+      testUtils.api.get('/v2/droplets/123/snapshots?page=1&per_page=1').reply(200, JSON.stringify(data));
 
-      client.droplets.actions(123, {
+      client.droplets.snapshots(123, {
         page: 1,
-        per_page: 2
-      }, function(err, actions, headers) {
-        expect(actions).to.be.eql(data.actions);
+        per_page: 1
+      }, function(err, snapshots, headers) {
+        expect(snapshots).to.be.eql(data.snapshots);
       });
     });
   });
+
+  describe('backups', function() {
+    var data = {
+      "backups": [
+        {
+          "id": 119192817,
+          "name": "Ubuntu 13.04",
+          "distribution": "ubuntu",
+          "slug": null,
+          "public": true,
+          "regions": [
+            "nyc1"
+          ],
+          "created_at": "2014-07-29T14:35:40Z",
+          "type": "backup"
+        }
+      ],
+      "meta": {
+        "total": 1
+      }
+    };
+
+    it('lists droplet backups', function() {
+      testUtils.api.get('/v2/droplets/123/backups').reply(200, JSON.stringify(data));
+
+      client.droplets.backups(123, function(err, backups, headers) {
+        expect(backups).to.be.eql(data.backups);
+      });
+    });
+
+    it('lists droplet backups at page', function() {
+      testUtils.api.get('/v2/droplets/123/backups?page=2').reply(200, JSON.stringify(data));
+
+      client.droplets.backups(123, 2, function(err, droplets, headers) {
+        expect(droplets).to.be.eql(data.backups);
+      });
+    });
+
+    it('lists droplet backups at page with length', function() {
+      testUtils.api.get('/v2/droplets/123/backups?page=2&per_page=1').reply(200, JSON.stringify(data));
+
+      client.droplets.backups(123, 2, 1, function(err, droplets, headers) {
+        expect(droplets).to.be.eql(data.backups);
+      });
+    });
+
+    it('lists droplet backups with a query object', function() {
+      testUtils.api.get('/v2/droplets/123/backups?page=1&per_page=1').reply(200, JSON.stringify(data));
+
+      client.droplets.backups(123, {
+        page: 1,
+        per_page: 1
+      }, function(err, backups, headers) {
+        expect(backups).to.be.eql(data.backups);
+      });
+    });
+  });
+
+  describe('neighbors', function() {
+    var data = {
+      "droplets": [
+        {
+          "id": 19,
+          "name": "test.example.com",
+          "memory": 1024,
+          "vcpus": 2,
+          "disk": 20,
+          "region": {
+            "slug": "nyc1",
+            "name": "New York",
+            "sizes": [
+              "1024mb",
+              "512mb"
+            ],
+            "available": true,
+            "features": [
+              "virtio",
+              "private_networking",
+              "backups",
+              "ipv6"
+            ]
+          },
+          "image": {
+            "id": 119192817,
+            "name": "Ubuntu 13.04",
+            "distribution": "ubuntu",
+            "slug": "ubuntu1304",
+            "public": true,
+            "regions": [
+              "nyc1"
+            ],
+            "created_at": "2014-07-29T14:35:36Z"
+          },
+          "size_slug": "1024mb",
+          "locked": false,
+          "status": "active",
+          "networks": {
+            "v4": [
+              {
+                "ip_address": "10.0.0.19",
+                "netmask": "255.255.0.0",
+                "gateway": "10.0.0.1",
+                "type": "private"
+              },
+              {
+                "ip_address": "127.0.0.19",
+                "netmask": "255.255.255.0",
+                "gateway": "127.0.0.20",
+                "type": "public"
+              }
+            ],
+            "v6": [
+              {
+                "ip_address": "2001::13",
+                "cidr": 124,
+                "gateway": "2400:6180:0000:00D0:0000:0000:0009:7000",
+                "type": "public"
+              }
+            ]
+          },
+          "kernel": {
+            "id": 485432985,
+            "name": "DO-recovery-static-fsck",
+            "version": "3.8.0-25-generic"
+          },
+          "created_at": "2014-07-29T14:35:36Z",
+          "features": [
+            "ipv6"
+          ],
+          "backup_ids": [
+            449676382
+          ],
+          "snapshot_ids": [
+            449676383
+          ],
+          "droplet_ids": [
+
+          ]
+        }
+      ],
+      "meta": {
+        "total": 1
+      }
+    };
+
+    it('lists droplet neighbors', function() {
+      testUtils.api.get('/v2/droplets/123/neighbors').reply(200, JSON.stringify(data));
+
+      client.droplets.neighbors(123, function(err, neighbors, headers) {
+        expect(neighbors).to.be.eql(data.droplets);
+      });
+    });
+
+    it('lists droplet neighbors at page', function() {
+      testUtils.api.get('/v2/droplets/123/neighbors?page=2').reply(200, JSON.stringify(data));
+
+      client.droplets.neighbors(123, 2, function(err, droplets, headers) {
+        expect(droplets).to.be.eql(data.droplets);
+      });
+    });
+
+    it('lists droplet neighbors at page with length', function() {
+      testUtils.api.get('/v2/droplets/123/neighbors?page=2&per_page=1').reply(200, JSON.stringify(data));
+
+      client.droplets.neighbors(123, 2, 1, function(err, droplets, headers) {
+        expect(droplets).to.be.eql(data.droplets);
+      });
+    });
+
+    it('lists droplet neighbors with a query object', function() {
+      testUtils.api.get('/v2/droplets/123/neighbors?page=1&per_page=1').reply(200, JSON.stringify(data));
+
+      client.droplets.neighbors(123, {
+        page: 1,
+        per_page: 1
+      }, function(err, neighbors, headers) {
+        expect(neighbors).to.be.eql(data.droplets);
+      });
+    });
+  });
+
 
   describe('delete', function() {
     it('deletes the droplet', function(done) {
